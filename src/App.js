c582c8a3fd75959a8dbc122f643e234e71759472
ghostrib <sh4rkbot@gmail.com>
2019-05-03 03:15:21 -0500

    adds server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SideNav from "./components/SideNav";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const getData = url => {
+//   fetch(url)
+//     .then(data => data.json())
+//     .then(json => console.log(json))
+//     .catch(error => console.error(error));
+// };
+
+class App extends Component {
+  state = {
+    todos: ""
+  };
+  getData = url => {
+    fetch(url)
+      .then(data => data.json())
+      .then(json =>
+        this.setState({
+          todos: json
+        })
+      )
+      .catch(error => console.error(error));
+  };
+
+  componentDidMount() {
+    this.getData("https://jsonplaceholder.typicode.com/todos/1");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SideNav />
+
+        <div className="main">
+          <h2>About me</h2>
+          <p>My name is Matt Brannon. I build stuff.</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SideNav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SideNav = () => (
+  <div className="sidenav">
+    <img
+      src="https://avatars2.githubusercontent.com/u/36570183"
+      alt="profile"
+    />
+    <a href="#">About</a>
+    <a href="https://github.com/ghostrib" target="_blank">
+      Github
+    </a>
+    <a href="mailto:sh4rkbot@gmail.com" target="_top">
+      Contact
+    </a>
+  </div>
+);
+
+export default SideNav;
