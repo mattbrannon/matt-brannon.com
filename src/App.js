1104a9becb9583ac1edb73ea2649e3aff3a0096b
ghostrib <sh4rkbot@gmail.com>
2019-05-09 03:17:00 -0500

    adds code example to homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import SideNav from "./components/SideNav";
 import "./App.css";
 
+function createPlayer(name, score) {
+  var player = {};
+  player.name = name;
+  player.score = score;
+  player.increment = function() {
+    this.score++;
+  };
+}
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +19,23 @@ class App extends Component {
         <div className="main">
           <h2>About me</h2>
           <p>My name is Matt Brannon. I build stuff.</p>
+          <h2>Instantiation patterns</h2>
+          <code>
+            <pre>
+              <p className="snippet">
+                {`
+  function createPlayer(name, score){
+    var player = {};
+    player.name = name;
+    player.score = score;
+    player.increment = function() {
+      this.score++;
+    };
+  }
+  `}
+              </p>
+            </pre>
+          </code>
         </div>
       </div>
     );
