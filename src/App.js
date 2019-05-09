1d19048d3afe29742c37d3801ad276b15234a9bc
ghostrib <sh4rkbot@gmail.com>
2019-05-09 02:43:48 -0500

    adds .env to gitignore

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import SideNav from "./components/SideNav";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    todos: ""
-  };
-  getData = url => {
-    fetch(url)
-      .then(data => data.json())
-      .then(json =>
-        this.setState({
-          todos: json
-        })
-      )
-      .catch(error => console.error(error));
-  };
-
-  componentDidMount() {
-    this.getData("https://jsonplaceholder.typicode.com/todos/1");
-  }
-
   render() {
     return (
       <div className="App">
