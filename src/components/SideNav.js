7c82475d7352b70f2a4829588f966fbdfd12beec
ghostrib <sh4rkbot@gmail.com>
2019-05-18 00:04:08 -0500

    fixes router

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,22 +2,22 @@ import React from "react";
 import { Router, Link } from "@reach/router";
 
 const SideNav = () => (
-  <Router>
-    <div className="sidenav">
-      <img
-        src="https://avatars2.githubusercontent.com/u/36570183"
-        alt="profile"
-      />
+  // <Router>
+  <div className="sidenav">
+    <img
+      src="https://avatars2.githubusercontent.com/u/36570183"
+      alt="profile"
+    />
 
-      {/* <a href="#">About</a> */}
-      <a href="https://github.com/ghostrib" target="_blank">
-        Github
-      </a>
-      <a href="mailto:sh4rkbot@gmail.com" target="_top">
-        Contact
-      </a>
-    </div>
-  </Router>
+    {/* <a href="#">About</a> */}
+    <a href="https://github.com/ghostrib" target="_blank">
+      Github
+    </a>
+    <a href="mailto:sh4rkbot@gmail.com" target="_top">
+      Contact
+    </a>
+  </div>
+  // </Router>
 );
 
 export default SideNav;
