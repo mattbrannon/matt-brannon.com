66bdacc580f2a6b1e3f0e0fcaf4b1fe7cdb0eeed
ghostrib <sh4rkbot@gmail.com>
2020-12-03 18:02:35 -0600

    update server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const path = require('path')
+const app = express()
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, '../build')))
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// app.get('/ping', (req, res) => {
+//   return res.send('pong')
+// })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+})
+
+app.listen(8080)
