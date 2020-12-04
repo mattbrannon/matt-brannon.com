4e9180f329130ddf202521617bf9258b411951dc
ghostrib <sh4rkbot@gmail.com>
2020-12-03 18:02:56 -0600

    update server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, '../build')))
 
-// app.get('/ping', (req, res) => {
-//   return res.send('pong')
-// })
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
