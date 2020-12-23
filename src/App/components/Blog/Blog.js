18bade53f855cb41bc08e79bf6db79c78489184a
ghostrib <sh4rkbot@gmail.com>
2020-12-23 17:27:19 -0600

    blog component

---
+++ src/App/components/Blog/Blog.js
@@ -0,0 +1,227 @@
+import s from "./Blog.module.css";
+
+const Content = () => {
+  return (
+    <div className={s.main}>
+      <div className={s.main__title}>
+        <h2 className={s.main__title__heading}>Blog Page</h2>
+      </div>
+
+      <article className={s.article}>
+        <h3 className={s.article__title}>article 1</h3>
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 1.1</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              earum, aut iusto eaque consectetur veniam, officiis, delectus
+              praesentium nihil sequi veritatis animi distinctio excepturi
+              laborum architecto obcaecati eos autem repellendus.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+              tenetur illo rem labore, iure commodi amet non! Delectus ex
+              dolores quis nesciunt voluptates minus quibusdam odit, debitis
+              dolorem assumenda enim.
+            </p>
+          </section>
+        </section>
+        <hr />
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 1.2</h4>
+
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+              est eaque, atque veniam laborum molestias deserunt veritatis
+              perspiciatis modi, delectus amet eveniet iure expedita quia,
+              dolorum beatae tenetur hic dolore.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+              autem maxime. Laborum eius earum nisi quisquam, ipsum eos a
+              placeat, voluptate sequi officiis dolore quis! Magnam eligendi
+              accusamus impedit repudiandae.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
+              enim molestiae labore optio, quas doloremque libero rem facilis
+              quaerat? Dolore eius neque provident, ex excepturi modi deleniti
+              architecto quo hic?
+            </p>
+          </section>
+        </section>
+        <hr />
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 1.3</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              ullam ipsum corrupti blanditiis necessitatibus nobis delectus
+              deleniti ut voluptas illo, dolorum nostrum corporis numquam nam
+              nulla praesentium debitis cupiditate! In?
+            </p>
+          </section>
+        </section>
+      </article>
+
+      <article className={s.article}>
+        <h3 className={s.article__title}>article 2</h3>
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 2.1</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              earum aut, incidunt commodi velit consequatur necessitatibus rerum
+              voluptates repudiandae recusandae! Quidem repellat debitis
+              sapiente distinctio deserunt quisquam ratione sunt laboriosam!
+            </p>
+          </section>
+        </section>
+        <hr />
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 2.2</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+              deleniti minus perferendis cumque hic reprehenderit eveniet
+              tenetur, libero dolor? Vitae eaque quo, atque debitis officia
+              mollitia voluptates reprehenderit rem iste.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              rerum molestiae reprehenderit labore, at dolorum ipsum numquam,
+              aperiam suscipit laboriosam, odit aspernatur! Aut, amet corporis
+              nihil quae inventore in eveniet?
+            </p>
+          </section>
+        </section>
+        <hr />
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 2.3</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              ea blanditiis facere voluptas sequi porro. Consequatur rerum
+              voluptatem tempore fugiat velit. Animi ab incidunt exercitationem
+              mollitia non, quae quasi dolorum.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              vitae at soluta illum distinctio quo ad hic sunt nobis! Corrupti
+              cumque quasi recusandae quaerat officiis exercitationem! Minima
+              soluta tempore dolore.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel at
+              voluptates, eos, illo hic expedita sint labore praesentium
+              incidunt odit corrupti, ea quas quis? At dolores nihil incidunt
+              sequi fugiat.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+              repellendus quia illum odit quam minus. Excepturi, eos, placeat
+              doloremque dicta magnam velit et quia eaque, eum cum nam ipsam
+              quisquam!
+            </p>
+          </section>
+        </section>
+        <hr />
+      </article>
+
+      <article className={s.article}>
+        <h3 className={s.article__title}>article 3</h3>
+
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 3.1</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              laborum cupiditate rerum dicta minima. Pariatur tempora eveniet
+              est quae accusamus aut officia! Reiciendis exercitationem,
+              repellat nisi ducimus atque esse sint.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus ea earum quam, quasi inventore suscipit perferendis
+              ipsam natus assumenda autem debitis nam voluptate et eius
+              voluptatibus rem dignissimos excepturi deserunt?
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Molestias nesciunt cumque quisquam porro modi dolor fuga ratione
+              blanditiis soluta. Dignissimos recusandae placeat facilis sit a
+              similique libero reiciendis, aperiam sint?
+            </p>
+          </section>
+        </section>
+        <hr />
+
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 3.2</h4>
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Provident, unde alias eaque doloremque corrupti porro eum maxime,
+              exercitationem, saepe iste consequatur consequuntur magnam omnis
+              fugiat vitae adipisci cum consectetur aliquid.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam
+              maxime maiores praesentium minus illo voluptatibus modi alias,
+              autem quo nihil ipsam numquam, culpa error vel sint corporis
+              beatae saepe aspernatur.
+            </p>
+            <p className="article__section__content__text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              tempore officia, magni culpa debitis iusto molestiae repellendus
+              consequatur? Consequatur tempore, enim velit similique quam
+              laudantium assumenda laboriosam ratione ea molestias!
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+              voluptate iure, impedit rem libero aut asperiores voluptates illum
+              cupiditate atque earum harum quasi veniam. At autem facere
+              deleniti sapiente omnis.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam
+              obcaecati assumenda est soluta nulla animi impedit earum quibusdam
+              quos nesciunt voluptas omnis quod doloremque fugiat, molestias
+              deserunt ipsum error ex!
+            </p>
+          </section>
+        </section>
+
+        <hr />
+
+        <section className={s.article__section}>
+          <h4 className={s.article__section__title}>section 3.3</h4>
+
+          <section className={s.article__section__content}>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+              facilis molestiae praesentium, doloremque esse laborum quibusdam
+              iusto in doloribus qui dicta illum maiores sequi error soluta
+              deleniti veritatis harum nisi.
+            </p>
+            <p className="article__section__content__text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
+              expedita aut pariatur placeat laborum amet aliquam ducimus quia,
+              molestias nobis ipsam numquam, a quidem obcaecati repudiandae
+              voluptatem ex. Reiciendis, placeat.
+            </p>
+            <p className="article__section__content__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+              fugiat, eius eligendi qui odio maxime molestias iure neque,
+              doloribus laudantium debitis sit blanditiis. Ea aliquid voluptate
+              velit a architecto eos?
+            </p>
+          </section>
+        </section>
+      </article>
+    </div>
+  );
+};
+
+export default Content;
