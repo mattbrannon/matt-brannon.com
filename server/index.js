d73c6c2a740a7d3bd1a24909a52b218d179801c9
ghostrib <sh4rkbot@gmail.com>
2020-12-23 21:11:33 -0600

    update server routes

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const routes = ["/", "/blog", "/apps", "/music", "/contact"];
+
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
-
-console.log(process.env.NODE_ENV, port);
+for (let route of routes) {
+  app.get(route, (_, res) => {
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+}
 
 app.listen(port);
