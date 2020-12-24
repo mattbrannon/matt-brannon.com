db0f945ec1efa89395332df2476b3f1730033582
ghostrib <sh4rkbot@gmail.com>
2020-12-23 20:28:55 -0600

    update env

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+require("dotenv").config();
 
-app.use(express.static(path.join(__dirname, '../build')))
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, "../build")));
 
-app.listen(8080)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+console.log(process.env.NODE_ENV, port);
+
+app.listen(port);
